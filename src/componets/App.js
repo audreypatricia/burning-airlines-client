86e0ca275293209cac3f95c63fcd331e69374931
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-12 10:40:25 +1000

    react route setup

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,11 +1,24 @@
 import SearchFlight from './SearchFlight';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navigation from './Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <p> Burning Airline Reservation</p>
-      <SearchFlight />
-    </div>
+    <Router>
+      <div className="App">
+      <Navigation />
+        <p> Burning Airline Reservation</p>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/search'>
+            <SearchFlight />
+          </Route>
+        </Switch >
+      </div>
+    </Router>
   );
 }
 
