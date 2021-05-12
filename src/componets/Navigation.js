0af7201f20221f84622e247bfca194d334804a18
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-12 16:04:57 +1000

    git fix please stop

--- src/componets/Navigation.js
+++ src/componets/Navigation.js
@@ -5,15 +5,15 @@ export default function Navigation() {
   return (
     <nav className="nav-bar">
       <Link to="/">Home</Link>
-<<<<<<< HEAD
-      <Link to='Search'>Search</Link>
-      <Link to='Search.:id'>Select Seat</Link>
-=======
+
+      
+
+
 
       <Link to="Search">Search</Link>
       <Link to="CreatePlane">CreatePlane</Link>
       <Link to="createFlight">CreatFlight</Link>
->>>>>>> 62003b6e25dc29a137eef12565b0a932abe00ef1
+
     </nav>
   );
 }
