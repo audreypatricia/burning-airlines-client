62003b6e25dc29a137eef12565b0a932abe00ef1
Qiong DAI(Nelly) <daiqiong524@outlook.com>
2021-05-12 15:42:38 +1000

    createFlight done

--- src/componets/CreateFlight.js
+++ src/componets/CreateFlight.js
@@ -1,40 +1,92 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+const SERVER_FLIGHTS_URL = "http://localhost:3000/flights.json";
 
 export class CreateFlight extends Component {
-    constructor() {
-        super();
-        this.state = {
-        flightnumber: '',
-        date: '',
-        departure: '',
-        arrival: '',
-        plane: ''
-      };
-      this._handleChange = this._handleChange.bind(this);
-      this._handleSubmit = this._handleSubmit.bind(this);
-    }
-
-    _handleSubmit(event){
-        event.preventDefault();
-        this.props.onSubmit( this.state)
-        this.setState({
-        flightNumber: '',
-        date: '',
-        departure: '',
-        arrival: '',
-        plane: ''
-      })
-    }
-
-    _handleChange(event) {
-      let value = event.target.value;
-      this.setState({[event.target.name]: value
-    });
-    console.log({[event.target.name]: value
-  });
+  constructor() {
+    super();
+    this.state = {
+      flightnumber: "",
+      date: "",
+      flight_from: "",
+      flight_to: "",
+      plane: "",
+    };
+    this._handleSubmit = this._handleSubmit.bind(this);
+    this._handleFlightnumber = this._handleFlightnumber.bind(this);
+    this._handleCreate = this._handleCreate.bind(this);
+    this._handleDeparture = this._handleDeparture.bind(this);
+    this._handleArrival = this._handleArrival.bind(this);
+    this._handleDate = this._handleDate.bind(this);
+    this._handlePlane = this._handlePlane.bind(this);
   }
 
+  componentDidMount() {
+    //Polling:
+    const fetchFligts = () => {
+      axios.get(SERVER_FLIGHTS_URL).then((results) => {
+        console.log(results);
+        this.setState({
+          flightnumber: results.data.flightnumber,
+          date: results.data.date,
+          flight_from: results.data.flight_from,
+          flight_to: results.data.flight_to,
+          plane: results.data.plane,
+        });
+        // setTimeout(fetchFligts, 4000); // recursion: setInterval is a luxury
+      });
+    };
+    fetchFligts();
+  }
 
+  _handleFlightnumber(event) {
+    this.setState((state) => ({ ...state, flightnumber: event.target.value }));
+  }
+
+  _handleDate(event) {
+    this.setState((state) => ({ ...state, date: event.target.value }));
+  }
+
+  _handleDeparture(event) {
+    this.setState((state) => ({ ...state, flight_from: event.target.value }));
+  }
+
+  _handleArrival(event) {
+    this.setState((state) => ({ ...state, flight_to: event.target.value }));
+  }
+
+  _handlePlane(event) {
+    this.setState((state) => ({ ...state, plane: event.target.value }));
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      flightNumber: "",
+      date: "",
+      flight_from: "",
+      flight_to: "",
+      plane: "",
+    });
+  }
+
+  _handleCreate(event) {
+    const data = {
+      flightnumber: this.state.flightnumber,
+      date: this.state.date,
+      flight_from: this.state.flight_from,
+      flight_to: this.state.flight_to,
+      plane: this.state.plane,
+    };
+    console.log(data);
+
+    axios.post(SERVER_FLIGHTS_URL, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
   render() {
     return (
@@ -44,30 +96,32 @@ export class CreateFlight extends Component {
           <label>
             Flight Number
             <input
-              type="number"
-              name="flightnumber"
+              onChange={this._handleFlightnumber}
               placeholder="Flight Number"
             />
           </label>
 
           <label>
             From
-            <input type="text" name="departure" placeholder="Departure City" />
+            <input
+              onChange={this._handleDeparture}
+              placeholder="Departure City"
+            />
           </label>
 
           <label>
             To
-            <input type="text" name="arrival" placeholder="Arrival City" />
+            <input onChange={this._handleArrival} placeholder="Arrival City" />
           </label>
 
           <label>
             Date
-            <input type="date" name="date" />
+            <input type="date" onChange={this._handleDate} />
           </label>
 
           <label>
             Plane:
-            <select name="plane">
+            <select onChange={this._handlePlane}>
               <option value="" selected>
                 -
               </option>
@@ -78,7 +132,11 @@ export class CreateFlight extends Component {
             </select>
           </label>
 
-          <button>Create Flight</button>
+          <input
+            onClick={this._handleCreate}
+            type="submit"
+            value="Create Flight"
+          />
         </form>
       </div>
     );
