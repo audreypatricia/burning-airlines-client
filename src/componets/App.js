8218ed0004dd1d11af1994feb7d83b03123fff06
Qiong DAI(Nelly) <daiqiong524@outlook.com>
2021-05-12 10:43:29 +1000

    createFlight first try

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,10 +1,12 @@
-import SearchFlight from './SearchFlight';
+import SearchFlight from "./SearchFlight";
+import CreateFlight from "./CreateFlight";
 
 function App() {
   return (
     <div className="App">
       <p> Burning Airline Reservation</p>
       <SearchFlight />
+      <CreateFlight />
     </div>
   );
 }
