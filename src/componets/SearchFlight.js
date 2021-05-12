c3f18b90dbdff798b452ed6c6e23e7ea94f96734
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-12 12:30:51 +1000

    Search Flight fixed to filter flights

--- src/componets/SearchFlight.js
+++ src/componets/SearchFlight.js
@@ -18,14 +18,20 @@ class SearchFlight extends Component {
       console.log(`fetching flights from ${from} to ${to}`);
       axios.get(SERVER_FLIGHTS_URL).then( (result) => {
         console.log(result.data);
-        this.setState({ flights: result.data });
+        console.log(result.data[0].airplane.name);
+        let filteredResults = [];
+        for(let i = 0; i < result.data.length; i++){
+          if(result.data[i].flight_from.toLowerCase() === from.toLowerCase() && result.data[i].flight_to.toLowerCase() === to.toLowerCase() ){
+            filteredResults.push(result.data[i]);
+          }
+        }
+        this.setState({ flights: filteredResults });
       });
   }
 
   render() {
     return(
       <div>
-        Search Flight feature coming soon
         <SearchForm onSubmit={ this.fetchFlights }/>
         <FlightList flights={ this.state.flights }/>
       </div>
